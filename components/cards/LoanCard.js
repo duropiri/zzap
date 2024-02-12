ae08099f0ae49317ea99ee58db80dd90760f0b31
Duro <duro.dp02@gmail.com>
2024-02-12 14:02:10 -0600

    eligibility complete, building loan page 2

--- components/cards/LoanCard.js
+++ components/cards/LoanCard.js
@@ -1,33 +1,46 @@
 import { View, Text } from "react-native";
 import React from "react";
+import Icon from 'react-native-vector-icons/FontAwesome'; 
 
 const LoanCard = ({ loan }) => {
+  // Define a function or a map to get the correct icon name based on the loan category
+  const getIconName = (category) => {
+    switch (category) {
+      case 'Education':
+        return 'graduation-cap'; // FontAwesome icon name
+      case 'Career':
+        return 'briefcase'; // FontAwesome icon name
+      // Add more cases for other categories
+      default:
+        return 'question-circle'; // Default icon
+    }
+  };
+
   return (
     <View className="m-4 p-4 rounded-lg border border-[#F8BFB2] bg-white flex-1">
-      <View className="flex-col justify-between">
-        <View>
-            {/* Add an icon that depends on the category of the loan */}
-          <Text className="text-xl text-[#031556] mb-2">
+      <View className="flex-row justify-between">
+        <Icon name={getIconName(loan.category)} size={30} color="#031556" />
+        <View className="flex-1">
+          <Text className="text-xl text-[#031556] mb-2 ml-2">
             {loan.type}
           </Text>
+          <Text className="text-5xl font-bold text-[#031556] my-4">
+            {loan.amount}
+          </Text>
         </View>
-
-        <Text className="text-5xl font-bold text-[#031556] my-4">
-          {loan.amount}
-        </Text>
       </View>
       <View className="flex-row justify-between">
-        <View className="flex-col justify-between mt-2">
+        <View className="flex-1">
           <Text className="text-[#031556]">Amount due</Text>
           <Text className="text-[#031556] font-bold">{loan.amountDue}</Text>
         </View>
-        <View className="flex-col justify-between mt-2">
+        <View className="flex-1">
           <Text className="text-[#031556]">Rate</Text>
           <Text className="text-[#031556] font-bold">{loan.rate}</Text>
         </View>
-        <View className="flex-col justify-between mt-2">
+        <View className="flex-1">
           <Text className="text-[#031556] text-right">Due date</Text>
-          <Text className="text-[#031556] font-bold">{loan.dueDate}</Text>
+          <Text className="text-[#031556] font-bold text-right">{loan.dueDate}</Text>
         </View>
       </View>
     </View>
